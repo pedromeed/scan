2968ee6bdd8db2a0ba7b8f7fd129159925c4636a
pedromeed <119433485+pedromeed@users.noreply.github.com>
2023-01-19 21:32:17 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var cols, rows;
+var scl = 20;
+var w = 1500;
+var h = 1000;
+angle = 0;
+var flying = 0;
+
+var terrain = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  cols = w / scl;
+  rows = h / scl;
+
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    }
+  }
+}
+
+function draw() {
+  
+  rotateX(angle);
+    angle += 0.007;
+  
+    
+
+  flying -= 0.1;
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 150);//ondulaçao do terreno
+      xoff += 0.2;
+    }
+    yoff += 0.2;
+  }
+
+
+  background(0);
+  translate(0, 50);
+  rotateX(PI / 3);
+ 
+  fill(245,6,78,80);
+  translate(-w / 2, -h / 2);
+  for (var y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 0.1) * scl, terrain[x][y + 1]);
+    }
+    endShape(noStroke())
+  }
+}
